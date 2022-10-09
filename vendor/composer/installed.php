95e702b64d9f356923c60d17f6d8e81bcd5abe19
snihirdima <snihirdima@gmail.com>
2022-10-09 13:48:59 +0300

    add heroku-php-apache2

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -10,6 +10,15 @@
         'dev' => true,
     ),
     'versions' => array(
+        'heroku/heroku-buildpack-php' => array(
+            'pretty_version' => 'v225',
+            'version' => '225.0.0.0',
+            'reference' => '6e2edde4512d5af97aa79ee6f42fe84620306164',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../heroku/heroku-buildpack-php',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
         'karan/shortenerdev' => array(
             'pretty_version' => '1.0.0+no-version-set',
             'version' => '1.0.0.0',
