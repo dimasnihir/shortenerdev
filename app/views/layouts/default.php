11dfe762e6db1eb6a93d7c13559c0a3cca639d2e
snihirdima <snihirdima@gmail.com>
2022-10-09 15:35:28 +0300

    update validation

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -72,13 +72,43 @@
             alertCopy.classList.remove('show');
         }, 1000)
     }
+    let buttonCopy = document.querySelector('#button-copy');
+    if (buttonCopy) {
+        buttonCopy.addEventListener('click', copy);
+    }
 
-    document.querySelector('#button-copy').addEventListener('click', copy);
 </script>
-<script src="../../../public/js/validation.js"></script>
 
+<script>
+    let form = document.querySelector('.js-validation');
+    let inputLink = document.querySelector('.js-longLink');
+
+    form.onsubmit = function () {
+
+        const isValidUrl = urlString=> {
+            let urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+                '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+                '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+                '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+            return !!urlPattern.test(urlString);
+        }
+
+        console.log(inputLink.value);
+        if (!isValidUrl(inputLink.value)) {
+            console.log('work');
+            let alertLinkError = document.querySelector('.alertErrorLink');
+            alertLinkError.classList.add('showErrorLink');
+
+            setTimeout(function(){
+                alertLinkError.classList.remove('showErrorLink');
+            }, 3000)
+
+            return false;
+        }
+
+    }
+</script>
 
 </html>
-<style>
-
-</style>
