dadccc7c5aa14700399164e2dd08df5e5260dc7f
snihirdima <snihirdima@gmail.com>
2022-10-10 09:19:25 +0300

    настройка деплоя

--- config_db.php
+++ config_db.php
@@ -8,8 +8,8 @@
 //];
 
 const DB_HOST = 'localhost';
-const DB_USER = 's112675_dbuser';
-const DB_PASSWORD = '9@hVE1o.~RseYMn.';
-const DB_NAME = 's112675_db';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = 'shortener';
 const CHARSET = 'utf8';
 const DB_PREFIX = '';
