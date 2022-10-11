7a5968b7696362de8d01b49cebc004306535c7b7
snihirdima <snihirdima@gmail.com>
2022-10-11 11:58:16 +0300

    Исправил ошибки

--- vendor/shortener/core/App.php
+++ vendor/shortener/core/App.php
@@ -12,7 +12,7 @@ class App
      public function __construct()
      {
           $query = trim($_SERVER['QUERY_STRING'], '/');
-//         session_start();
+         session_start();
 
          self::$app = Registry::instance();
          $this->getParams();
