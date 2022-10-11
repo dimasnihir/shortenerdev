907d2a1970b9fefa1bf467b844688de4cbdcbafd
snihirdima <snihirdima@gmail.com>
2022-10-11 11:07:17 +0300

    Исправил ошибки

--- app/controllers/RedirectController.php
+++ app/controllers/RedirectController.php
@@ -6,15 +6,17 @@ namespace app\controllers;
 
 use app\models\ShortLink;
 use shortener\base\Controller;
-use shortener\DataBase;
 
 class RedirectController extends Controller
 {
 
+
     // urlshortener.space/8461d
     public function indexAction() {
+        var_dump($this->route);
 
         $shortLink = $_SERVER['HTTP_HOST'] . '/' . $this->route['token'];
+        var_dump($shortLink);
         if (!ShortLink::isNotShortLink($shortLink)) {
             $longLink = ShortLink::getLongLink($shortLink);
             header("Location: {$longLink}");
