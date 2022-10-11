6f31f98d1eaa180b6b005177beddf099f40e00bb
snihirdima <snihirdima@gmail.com>
2022-10-11 11:32:42 +0300

    Исправил ошибки

--- app/controllers/RedirectController.php
+++ app/controllers/RedirectController.php
@@ -9,18 +9,14 @@ use shortener\base\Controller;
 
 class RedirectController extends Controller
 {
-
-
-    // urlshortener.space/8461d
     public function indexAction() {
-//        var_dump($this->route);
-
         $shortLink = $_SERVER['HTTP_HOST'] . '/' . $this->route['token'];
-//        var_dump($shortLink);
         if (!ShortLink::isNotShortLink($shortLink)) {
             $longLink = ShortLink::getLongLink($shortLink);
-//            var_dump($longLink);
+            ob_start();
             header("Location: https://github.com/dimasnihir/ishop");
+
+//            header("Location: {$longLink}");
         } else {
             throw new \Exception("Страница не найдена", 404);
         }
