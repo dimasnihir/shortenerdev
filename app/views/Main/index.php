ef7a5bb22eb7bc6c108b08559e1c39423b4a44cb
snihirdima <snihirdima@gmail.com>
2022-10-12 16:02:47 +0300

    Исправляю баги

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -48,3 +48,34 @@
 
 </section>
 <!-- End section-form -->
+<script>
+    let form = document.querySelector('.js-validation');
+    let inputLink = document.querySelector('.js-longLink');
+
+    form.onsubmit = function () {
+
+        const isValidUrl = urlString=> {
+            let urlPattern = new RegExp('^(https?:\\/\\/)'+ // validate protocol
+                '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+                '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+                '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+                '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+                '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+            return !!urlPattern.test(urlString);
+        }
+
+        console.log(inputLink.value);
+        if (!isValidUrl(inputLink.value)) {
+            console.log('work');
+            let alertLinkError = document.querySelector('.alertErrorLink');
+            alertLinkError.classList.add('showErrorLink');
+
+            setTimeout(function(){
+                alertLinkError.classList.remove('showErrorLink');
+            }, 3000)
+
+            return false;
+        }
+
+    }
+</script>
